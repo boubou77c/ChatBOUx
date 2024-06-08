c1b77904fccf54960c89afb25b63dc26e19a6ce7
boubou77c <142178273+boubou77c@users.noreply.github.com>
2024-06-08 21:25:03 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module boubou.chatbox.chatboxui {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens boubou.chatbox.chatboxui to javafx.fxml;
-    exports boubou.chatbox.chatboxui;
-}
